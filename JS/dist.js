a94578694a493aa260aef3440519e82ad3ee76c6
Kunwar Parth Singh <parthsingh287@icloud.com>
2018-01-08 01:58:05 +0530

    Computer screen text fixed

--- JS/dist.js
+++ JS/dist.js
@@ -8,7 +8,7 @@
   	$(".bkgmove").paroller({factor:'0.3',type:'foreground',direction:'vertical'});
 	$("#comp0 img").paroller({ factor: '0.5', type: 'foreground', direction: 'horizontal' });
 	$("h1").paroller({ factor: '0.5', type: 'foreground'});
-	$("header.secscreen").paroller({ factor: '-1.7', type: 'foreground', direction: 'horizontal' });
+	$("header.secscreen").paroller({ factor: '-1', type: 'foreground', direction: 'horizontal' });
 	$("footer.secscreen").paroller({ factor: '-0.3', type: 'foreground', direction: 'horizontal' });
 	$(".block:odd").paroller({ factor: '0.2', type: 'foreground', direction: 'horizontal' });
 	$(".block:even").paroller({ factor: '-0.2', type: 'foreground', direction: 'horizontal' });
@@ -34,7 +34,7 @@
 // 	$("#About").paroller({ factor: '0', type: 'foreground', direction: 'horizontal' });
 
 //     	}
-//     });
+    // });
 // $(window).resize(function() {
 //     if( $(this).width() <500 ) {
 //         $(function () {
