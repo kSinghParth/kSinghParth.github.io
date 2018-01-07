5221e428dbe34fb5c2010b53703a5f26e48b76e8
Kunwar Parth Singh <parthsingh287@icloud.com>
2018-01-08 02:02:12 +0530

    Parallax Scroll fixes

--- JS/dist.js
+++ JS/dist.js
@@ -3,8 +3,6 @@
 
 	$("body").paroller();
 	$("#comp2").paroller({ factor: '1.0', type: 'foreground', direction: 'horizontal' });
-	$(".codesnip").paroller({ factor: '0.1', type: 'foreground', direction: 'horizontal' });
-  	$("#logo").paroller({ factor: '-0.2', type: 'foreground' });
   	$(".bkgmove").paroller({factor:'0.3',type:'foreground',direction:'vertical'});
 	$("#comp0 img").paroller({ factor: '0.5', type: 'foreground', direction: 'horizontal' });
 	$("h1").paroller({ factor: '0.5', type: 'foreground'});
